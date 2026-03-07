2d828c093d71964517822addad0d931b999b473a
JohanGM2003 <jgm944@educa.madrid.org>
2026-03-07 10:39:53 +0100

    Primer proyecto Java

--- src/Basicos/MiPrimerHolaMundo.java
+++ src/Basicos/MiPrimerHolaMundo.java
@@ -3,6 +3,31 @@ package Basicos;
 public class MiPrimerHolaMundo {
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo");
+
+        String saludar = "Hola mundo";
+        System.out.println(saludar);
+        System.out.println("Saludar.toUpperCase() = " + saludar.toUpperCase() );
+
+        int numero = 11;
+
+        boolean valor = true;
+        int numero2 = 5;
+
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2=10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "coccococococ";
+
+        String nombre = "Carlos";
+
+       if(numero>10){
+           nombre = "Johan";
+       }
+
+        System.out.println("nombre = " + nombre);
+
     }
 }
---
+++ src/Basicos/Primitivos.java
@@ -0,0 +1,13 @@
+package Basicos;
+
+public class Primitivos {
+    public static void main(String[] args) {
+
+        byte numerobyte = 127;
+        System.out.println("numerobyte = " + numerobyte);
+        System.out.println("Tipo byte corresponde en byte a " + Byte.BYTES);
+        System.out.println("Tipo de byte corresponde en bites a " + Byte.SIZE);
+        System.out.println("Valor máximo de un Byte " + Byte.MAX_VALUE);
+        System.out.println("Valor mínimo de un Byte " + Byte.MIN_VALUE);
+    }
+}
